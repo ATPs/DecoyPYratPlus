7ba9c05bdaa8cb81dcc6a94f7b4fec5b28bfa105
Xiaolong Cao <ATPs@users.noreply.github.com>
2023-03-31 16:31:38 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@ from setuptools import setup
 version = '1.0.1'
 
 setup(
-    name = 'decoypyrat',
+    name = 'DecoyPYratPlus',
     version = version,
-    packages = ['decoypyrat'],
+    packages = ['DecoyPYratPlus'],
     description = 'Fast Hybrid Decoy Sequence Database Creation for Proteomic Mass Spectrometery Analyses',
-    author = 'James Wright',
+    author = 'James Wright, Xiaolong Cao',
     author_email = 'james.wright@sanger.ac.uk',
-    url = 'https://github.com/wtsi-proteomics/DecoyPYrat',
+    url = 'https://github.com/ATPs/DecoyPYratPlus',
     keywords = ['database searching',' fdr',' python',' sequence database',' shotgun proteomics',' target-decoy'],
-    install_requires=[
+    install_requires=['numpy',
     ],
     license = 'MIT',
     entry_points={
         'console_scripts': [
-            'decoypyrat = decoypyrat.decoyPYrat:main'
+            'DecoyPYratPlus = decoypyrat.decoyPYratPlus:main'
         ]
     }
 )
